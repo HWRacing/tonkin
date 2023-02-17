f3f237ff06e8394e417e94687515c5d6756e6615
Hamish Knox <hrjkknox@gmail.com>
2023-02-17 23:22:54 +0000

    Add code to get location of sub-arrays within arrays

--- src/tonkin/arrays.py
+++ src/tonkin/arrays.py
@@ -6,3 +6,18 @@ def shiftArrayLeft(array, shift):
 	for i in range(0, shift):
 		array.append(array.pop(0))
 	return array
+
+# Given a source array and a target array, returns the index of where the target
+# array appears within the source array (returns the index of the final value)
+def getSubArrayIndex(source, target):
+	# TODO: Ensure that the buffer items don't appear in split
+	buffer = [None] * len(target)
+	
+	for j, i in enumerate(source):
+		buffer = shiftArrayLeft(buffer, 1)
+		buffer[-1] = i
+
+		if buffer == target:
+			return j
+	
+	raise ValueError("Sub-array not found")
