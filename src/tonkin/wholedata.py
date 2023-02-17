fe13359f277ab4884c3a86cc8d3e5fb5a28afd0c
Hamish Knox <hrjkknox@gmail.com>
2023-02-17 23:33:05 +0000

    Update code to extract variables section

--- src/tonkin/wholedata.py
+++ src/tonkin/wholedata.py
@@ -1,28 +1,10 @@
+from .arrays import getSubArrayIndex
+
 # Returns the header section of a file
 def extractHeaderSec(fileBytes):
 	return fileBytes[0:228]
 
-# Get the index of the end of the variable definition section
-# (as signalled by the bytes 43 47 1A)
-def getEndOfVariableSec(bytesWithoutHeader):
-	# Buffer of three bytes
-	buffer = [0x00, 0x00, 0x00]
-	
-	for j, i in enumerate(bytesWithoutHeader):
-		# Push bytes back one
-		buffer[0] = buffer[1]
-		buffer[1] = buffer[2]
-
-		# Get new byte
-		buffer[2] = i
-		print(buffer)
-
-		if buffer == [0x43, 0x47, 0x1A]:
-			return j
-	
-	raise ValueError("Variable definition end bytes not found")
-
 def extractVariablesSec(fileBytes):
 	output = fileBytes[228:]
-	end = getEndOfVariableSec(output)
+	end = getSubArrayIndex(output, [0x43, 0x47, 0x1A])
 	return output[:end]
