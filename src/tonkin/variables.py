f404d565756b8a735b0c7dbe7d92c41ded852595
Hamish Knox <hrjkknox@gmail.com>
2023-02-18 01:20:03 +0000

    Add code to extract variable name from definition block

--- src/tonkin/variables.py
+++ src/tonkin/variables.py
@@ -2,3 +2,16 @@ from .arrays import splitArrayBySub
 
 def splitVariables(variableSection):
 	return splitArrayBySub(variableSection, [0x43, 0x43, 0x3E])
+
+def extractVariableName(variableDefinition):
+	outputBytes = []
+	# Remove data before variable name
+	variableDefinition = variableDefinition[66:]
+
+	# Extract name
+	for i in variableDefinition:
+		if i == 0x00:
+			break
+		outputBytes.append(chr(i))
+
+	return "".join(outputBytes)
