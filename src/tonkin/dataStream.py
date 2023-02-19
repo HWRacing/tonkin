1e9eedc1fe87be3121ac0985f3ba7882165cbd49
Hamish Knox <hrjkknox@gmail.com>
2023-02-19 16:19:19 +0000

    Decouple code

--- src/tonkin/dataStream.py
+++ src/tonkin/dataStream.py
@@ -1,16 +1,12 @@
-import data
+import variables
 import arrays
 
-def calculatePacketLength(variables):
+def calculatePacketLength(vars):
 	# Starts at 4 because time is stored in a single
-	packetLength = 4
-	for variable in variables:
-		length = data.typeLengths[variable["Type"]]
-		packetLength += length
-	return packetLength
+	return 4 + variables.getVariableLengths(vars)
 
 # Given a data stream and the list of variable types,
 # get a list of packets from the data stream
-def splitStreamToPackets(stream, variables):
-	packetLength = calculatePacketLength(variables)
+def splitStreamToPackets(stream, vars):
+	packetLength = calculatePacketLength(vars)
 	return arrays.splitArrayIntoLengths(stream, packetLength)
