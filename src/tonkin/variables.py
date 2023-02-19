46abcb6ce7cfcc8a7ac7754d0bd1dc51bfdc5d2a
Hamish Knox <hrjkknox@gmail.com>
2023-02-19 01:57:41 +0000

    Add code for extracting variables from A2L data

--- src/tonkin/variables.py
+++ src/tonkin/variables.py
@@ -15,3 +15,23 @@ def extractVariableName(variableDefinition):
 		outputBytes.append(chr(i))
 
 	return "".join(outputBytes)
+
+# Temporary workaround because I haven't figured out
+# how types are encoded in the EcoCAL data format
+def getTypeFromA2LData(variableName, a2lData):
+	for i in a2lData:
+		rightCategory = i["Category"] == "Measurement"
+		rightName = i["Name"] == variableName
+		if rightCategory and rightName:
+			return i["Type"]
+	raise ValueError("Variable " + variableName + " not found in A2L data")
+
+def matchWithA2LData(variableNames, a2lData):
+	output = []
+	for variable in variableNames:
+		currentVar = {
+			"Name": variable,
+			"Type": getTypeFromA2LData(variable, a2lData)
+		}
+		output.append(currentVar)
+	return output
