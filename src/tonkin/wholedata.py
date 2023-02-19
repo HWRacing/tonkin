b65d896e47c495ce1fded1fcc47f63f6d5bf7a49
Hamish Knox <hrjkknox@gmail.com>
2023-02-19 02:36:20 +0000

    Add code to extract data stream

--- src/tonkin/wholedata.py
+++ src/tonkin/wholedata.py
@@ -8,3 +8,21 @@ def extractVariablesSec(fileBytes):
 	output = fileBytes[228:]
 	end = arrays.getSubArrayIndex(output, [0x43, 0x47, 0x1A])
 	return output[:end]
+
+def extractDataStream(fileBytes, prePrepared=False):
+	# Remove the header and variable definition sections
+	# if it hasn't been done already
+	if prePrepared:
+		output = fileBytes
+	else:
+		headerLength = len(extractHeaderSec(fileBytes))
+		variableLength = len(extractVariablesSec(fileBytes))
+		upperLength = headerLength + variableLength
+		output = fileBytes[upperLength:]
+
+	# The sequence of bytes that marks the start of the data stream
+	startMarker = [0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+	# The index of the first byte of the data stream
+	start = arrays.getSubArrayIndex(output, startMarker) + 1
+	
+	return output[start:]
