aa197087ff846597f0f7b6b7dc750f6a9888803a
Hamish Knox <hrjkknox@gmail.com>
2023-02-19 17:31:25 +0000

    Code cleanup

--- src/tonkin/dataStream.py
+++ src/tonkin/dataStream.py
@@ -1,12 +1,12 @@
 import variables
 import arrays
 
-def calculatePacketLength(vars):
+def _calculatePacketLength(vars):
 	# Starts at 4 because time is stored in a single
 	return 4 + variables.getVariableLengths(vars)
 
 # Given a data stream and the list of variable types,
 # get a list of packets from the data stream
 def splitStreamToPackets(stream, vars):
-	packetLength = calculatePacketLength(vars)
+	packetLength = _calculatePacketLength(vars)
 	return arrays.splitArrayIntoLengths(stream, packetLength)
