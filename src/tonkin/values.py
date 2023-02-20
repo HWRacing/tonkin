2cb323358135db6115811f57cb2bf15ec465943c
Hamish Knox <hrjkknox@gmail.com>
2023-02-20 03:04:09 +0000

    Add code to convert values

---
+++ src/tonkin/values.py
@@ -0,0 +1,50 @@
+import struct
+
+def _convertint8(int8B):
+	return int.from_bytes(int8B, "little", signed=True)
+
+def _convertint16(int16B):
+	return int.from_bytes(int16B, "little", signed=True)
+
+def _convertint32(int32B):
+	return int.from_bytes(int32B, "little", signed=True)
+
+def _convertuint8(uint8B):
+	return int.from_bytes(uint8B, "little", signed=False)
+
+def _convertuint16(uint16B):
+	return int.from_bytes(uint16B, "little", signed=False)
+
+def _convertSingle(singleB):
+	return struct.unpack("f", singleB)
+
+def _convertBoolean(boolB):
+	if boolB == b'\x01':
+		return True
+	else:
+		return False
+
+def _convertFixed(fixedB):
+	pass
+
+# Given a series of bytes representing a value within a packet
+# and a variable dict, return the value of the 
+def readRawValue(rawValue, var):
+	varType = var["Type"]
+	
+	if varType == "int8":
+		return _convertint8(rawValue)
+	elif varType == "int16":
+		return _convertint16(rawValue)
+	elif varType == "int32":
+		return _convertint32(rawValue)
+	elif varType == "uint8":
+		return _convertuint8(rawValue)
+	elif varType == "uint16":
+		return _convertuint16(rawValue)
+	elif varType == "single":
+		return _convertSingle(rawValue)
+	elif varType == "boolean":
+		return _convertBoolean(rawValue)
+	elif varType == "fixed":
+		return _convertFixed(rawValue)
