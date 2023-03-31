2f82a4c76b93ad9947a602ad7af77092abc5a59d
Hamish Knox <hrjkknox@gmail.com>
2023-03-31 19:19:30 +0100

    Remove duplicate data

--- src/tonkin/datvar.py
+++ src/tonkin/datvar.py
@@ -3,6 +3,5 @@ from dataclasses import dataclass
 # A variable as it appears in the header section
 @dataclass
 class Datvar:
-    name: str
-    dataType: str
-    byteLength: int
+	name: str
+	dataType: str
