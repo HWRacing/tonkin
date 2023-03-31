f33cd385538bbb57bd15886bd612a959390fa6d0
Hamish Knox <hrjkknox@gmail.com>
2023-03-31 19:46:59 +0100

    Finish code to extract stuff from variable section

--- src/tonkin/varsec.py
+++ src/tonkin/varsec.py
@@ -5,11 +5,11 @@ from countach import measurement as mea
 from countach import characteristic as cha
 
 # Split the variables section into individual definitions
-def splitVarSectionToDefs(varSection: bytearray) -> List[List[bytearray]]:
+def splitVarSectionToDefs(varSection: bytearray) -> List[List[int]]:
 	return lops.splitListBySub(list(varSection), [0x43, 0x43, 0x3E])
 
 # Extract a variable name from a definition
-def extractVariableName(varDef: bytearray) -> str:
+def extractVariableName(varDef: List[int]) -> str:
 	outputBytes = []
 	# Remove data before variable name
 	varDef = varDef[66:]
@@ -23,7 +23,7 @@ def extractVariableName(varDef: bytearray) -> str:
 	return "".join(outputBytes)
 
 # Get a list of variable names from a list of definitions (ignores the last one)
-def extractVariableNames(varDefs: List[bytearray]) -> List[str]:
+def extractVariableNames(varDefs: List[List[int]]) -> List[str]:
 	# Don't get the "time" variable
 	varDefs.pop(-1)
 	output = []
@@ -39,3 +39,14 @@ def getDatVarFromA2L(variableName: str, a2lData: List[Union[mea.Measurement, cha
 		if rightCategory and rightName:
 			return datvar.Datvar(variableName, i.dataType)
 	raise ValueError("Variable " + variableName + " not found in A2L data")
+
+def getDatVarsFromA2L(variableNames: List[str], a2lData: List[Union[mea.Measurement, cha.Characteristic]]) -> List[datvar.Datvar]:
+	output = []
+	for variableName in variableNames:
+		output.append(getDatVarFromA2L(variableName, a2lData))
+	return output
+
+def getVariableListFromA2L(varSection: bytearray, a2lData: List[Union[mea.Measurement, cha.Characteristic]]) -> List[datvar.Datvar]:
+	defs = splitVarSectionToDefs(varSection)
+	variableNames = extractVariableNames(defs)
+	return getDatVarsFromA2L(variableNames, a2lData)
